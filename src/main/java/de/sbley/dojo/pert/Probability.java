3f9735fe0576c6a1cad3c622615074cac9580b16
Stefan Bley <stefan.bley@saxsys.de>
2011-07-31 21:59:15 +0200

    make Probability package visible

--- src/main/java/de/sbley/dojo/pert/Probability.java
+++ src/main/java/de/sbley/dojo/pert/Probability.java
@@ -1,6 +1,6 @@
 package de.sbley.dojo.pert;
 
-public class Probability {
+class Probability {
 
 	public static float expectation(int optimistic, int nominal, int pessimistic) {
 		return (optimistic + 4 * nominal + pessimistic) / 6F;
