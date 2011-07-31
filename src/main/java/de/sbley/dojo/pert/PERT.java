c2f708c13cdaadf062e3d6c9b6e0f6af8d28175a
Stefan Bley <stefan.bley@saxsys.de>
2011-07-31 22:17:48 +0200

    10th test: PERT class can read input values from a Reader (to make it readable from console)

--- src/main/java/de/sbley/dojo/pert/PERT.java
+++ src/main/java/de/sbley/dojo/pert/PERT.java
@@ -1,11 +1,19 @@
 package de.sbley.dojo.pert;
 
+import java.io.Reader;
+import java.util.Scanner;
+
 public class PERT {
 
 	public Estimate estimate(int optimistic, int nominal, int pessimistic) {
 		return new Estimate(Probability.expectation(optimistic, nominal,
 				pessimistic), Probability.deviation(optimistic, nominal,
 				pessimistic));
+	}
+
+	public Estimate estimate(Reader reader) {
+		Scanner scanner = new Scanner(reader);
+		return estimate(scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
 	}
 
 }
--- src/test/java/de/sbley/dojo/pert/PERTTest.java
+++ src/test/java/de/sbley/dojo/pert/PERTTest.java
@@ -3,12 +3,21 @@ package de.sbley.dojo.pert;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.io.StringReader;
+
 import org.junit.Test;
 
 public class PERTTest {
 	@Test
 	public void estimateContainsExpectationAndDeviation() throws Exception {
 		Estimate e = new PERT().estimate(1, 2, 3);
+		assertThat(e.getExpectation(), is(2F));
+		assertThat(e.getDeviation(), is(1 / 3F));
+	}
+
+	@Test
+	public void estimateReadsInputValuesFromAReader() throws Exception {
+		Estimate e = new PERT().estimate(new StringReader("1 2 3"));
 		assertThat(e.getExpectation(), is(2F));
 		assertThat(e.getDeviation(), is(1 / 3F));
 	}
